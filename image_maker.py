4c2bcae7f7ff6061fe19cb632b284cf0e87a46ba
Benjamin Fair <fair.105@osu.edu>
2015-11-15 02:03:25 -0500

    Wrap between words in image_maker

--- image_maker.py
+++ image_maker.py
@@ -12,12 +12,15 @@ offset = 0
 textStr = "QWERTYUIOPASDFGHJKLZXCVBNMQWERTYUIOPSDFGHJKLZXCVBNMQWERTYUIOPASDFGHJKL" 
 
 def maxCharPerLine(width):
-  return int(round((width/8.33),0)-3)
+  return int(round((width/8.33),0))
+
+def getMaxLines(height):
+  return int(round(height/fontHeight,0))
 
 def pick():
   returnThis = []
   for z in range(0, len(x)/2):
-    maxLines = int(round((y[z+1]-y[z])/fontHeight,0))
+    maxLines = getMaxLines(y[z+1]-y[z])
     print maxLines
     cpl = maxCharPerLine(x[z+1]-x[z])
     print maxCharOnLine  
@@ -27,10 +30,9 @@ def pick():
 
 def make(comic, dialog):
   for n in range(len(dialog)):
-    text = dialog[n]
     cpl = maxCharPerLine(x[n+1]-x[n])
-    totLines = len(text)/cpl+1
-    print totLines
+    maxLines = getMaxLines(y[n+1]-y[n])
+    print maxLines
     im = Image.open("%d.png" % comic)
     draw = ImageDraw.Draw(im)
 
@@ -38,16 +40,25 @@ def make(comic, dialog):
     draw.rectangle([(x[n],y[n]),(x[n+1],y[n+1])], fill=255, outline=None)
 
     #writes new text at given position
-    for v in range(0, totLines):      
-      draw.text((x[n],y[n]+fontHeight*v), text[cpl*v:(cpl*(v+1))], font=font)
+    words = dialog[n].split(' ')
+    curWord = 0
+    for v in range(0, maxLines):      
+      startWord = curWord
+      curChars = 0
+      while curWord < len(words):
+        curChars += len(words[curWord])+1
+        if curChars > cpl:
+          break
+        curWord += 1
+      draw.text((x[n],y[n]+fontHeight*v), ' '.join(words[startWord:curWord]), font=font)
     del draw
-    # write to stdout
+    # write to output
     with open("out.png", 'w') as f:
       im.save(f, "PNG")
 
   return "out.png"
 
-
-
-
-
+if __name__ == "__main__":
+  num, lengths = pick()
+  print num, lengths
+  print make(num, ["a" * length for length in lengths])
